22b41848d66e59fcd431aaa3399915f770d752e1
PizzaReaver <rose0188@algonquinlive.com>
2016-06-09 21:52:57 -0400

    Minor Push

---
+++ Project_BR/Assets/_Main/Scripts/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+    public Sprite[] Logos;
+    public float fadeTime;
+    public Color ImageHolderColor;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private IEnumerator CycleLogo_cr(float fadeTime, Sprite Logo, Color ImageHolderColor)
+    {
+        //ImageHolder
+        yield return null;
+    }
+}
